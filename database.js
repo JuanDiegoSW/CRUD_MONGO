1a9f8f991fb3d2a3940d33ddcd31e11108fa5a8f
JuanDiegoSW <juan.lazaro.m@tecsup.edu.pe>
2021-10-06 10:27:55 -0500

    fix fix

--- database.js
+++ database.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 //const { connect } = require('./server')
-const MONGODB_URI = 'mongodb+srv://admin2021:admin2021@cluster0.b4feb.mongodb.net/database'
+//const MONGODB_URI = 'mongodb+srv://admin2021:admin2021@cluster0.b4feb.mongodb.net/database'
 //const MONGODB_URI = 'mongodb+srv://admindb:12345@dbmyfirstapp.sqjx6.mongodb.net/DBTest'
-
+const MONGODB_URI = 'mongodb+srv://root:root@cluster0.b4feb.mongodb.net/database'
 
 //authSource=admin&replicaSet=atlas-wjvtc8-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true
 
